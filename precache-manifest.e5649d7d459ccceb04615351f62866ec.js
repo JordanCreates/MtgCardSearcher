34d35e93441725bb383f54f6afa658939eaa632a
Jordan <jordanyu@hotmail.co.uk>
2019-09-11 14:46:45 +0100

    Updates

---
+++ precache-manifest.e5649d7d459ccceb04615351f62866ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a909890b92d92d223e7a5605bb0bc817",
+    "url": "/MtgCardSearcher/index.html"
+  },
+  {
+    "revision": "3fd09fe188910b2dcc2f",
+    "url": "/MtgCardSearcher/static/css/main.46139178.chunk.css"
+  },
+  {
+    "revision": "267b067dc44ce7dd5282",
+    "url": "/MtgCardSearcher/static/js/2.d71abc2d.chunk.js"
+  },
+  {
+    "revision": "3fd09fe188910b2dcc2f",
+    "url": "/MtgCardSearcher/static/js/main.91f4d1a9.chunk.js"
+  },
+  {
+    "revision": "1ca29bfaea5be3c0dc40",
+    "url": "/MtgCardSearcher/static/js/runtime~main.ab9f5e17.js"
+  }
+]);
